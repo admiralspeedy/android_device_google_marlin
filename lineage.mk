954c7169bcbceabbb161d8c32f5b550ac22cba0d
Mitchell <admrlspeedy@hotmail.com>
2017-07-15 19:18:54 -0600

    marlin/sailfish: Product manufacturer google -> Google
    Change-Id: I301c563a64076f759d8aaed4b0965becbec14615

--- lineage.mk
+++ lineage.mk
@@ -12,7 +12,7 @@ $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_marlin
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := HTC
 PRODUCT_RESTRICT_VENDOR_FILES := false
